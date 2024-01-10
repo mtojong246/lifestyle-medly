a21c35da98c631fa0e3e9d79886e281ed4058782
Melanie Tojong <mtojong@Melanies-MBP.lan>
2024-01-09 19:41:16 -0500

    adding first page

---
+++ src/app/Components/Navigation.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Logo from '../../../public/lifestyle_logo.png';
+import Link from 'next/link';
+import Hamburger from './Hamburger';
+import { useState, MouseEvent } from 'react';
+
+export default function Navigation() {
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleOpen = (e:MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <div className='w-full'>
+            <div className="max-w-[1400px] my-0 mx-auto p-4 flex justify-between items-center gap-8">
+                <div className='flex justify-center items-center gap-8'>
+                    <img className='md:w-[50px] md:h-[50px] w-[100px] h-[100px]' src='/lifestyle_logo.png' />
+                    <Link className='hidden md:block' href='#'>How It Works</Link>
+                    <Link className='hidden md:block' href='#'>Direct Pay</Link>
+                    <Link className='hidden md:block' href='#'>FAQ</Link>
+                </div>
+                <div className='flex justify-center items-center gap-8'>
+                    <Link className='hidden md:block' href='#'>For Patients</Link>
+                    <Link className='hidden md:block' href='#'>Blog</Link>
+                    <Link className='hidden md:block' href='#'>Contact</Link>
+                    <button className='py-2 px-6 rounded-full bg-charcoal text-white hidden md:block'>Get Started</button>
+                    <button onClick={toggleOpen} className='block md:hidden'><Hamburger isOpen={isOpen}/></button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,38 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'charcoal': '#303030',
+        'maize': '#F0C453',
+        'gold': '#AB8100',
+        'vanilla': '#545454',
+      }
     },
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      md: ['18px', '26px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '870px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
+    
+    
   },
   plugins: [],
 }
